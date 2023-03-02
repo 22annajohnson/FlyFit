30926354fe4b3786b73e925c3334dd80590ddb5b
Anna Johnson <22annajohnson@gmail.com>
2023-03-02 14:47:37 -0500

    Test commit

--- FlyFit/ViewControllers/HomeViewController.swift
+++ FlyFit/ViewControllers/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemRed
         // Do any additional setup after loading the view.
     }
     
