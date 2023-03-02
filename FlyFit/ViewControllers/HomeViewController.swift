67e547ad1d317f14d2ebf76f175f8b7a17292c05
Jake-loranger <106925875+Jake-loranger@users.noreply.github.com>
2023-03-02 14:35:08 -0500

    Initial Commit

---
+++ FlyFit/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  FlyFit
+//
+//  Created by Anna Johnson on 3/2/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlyFit/ViewControllers/MainTabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarViewController.swift
+//  FlyFit
+//
+//  Created by Anna Johnson on 3/2/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc1.title = "Hanger"
+        
+        
+        
+        tabBar.tintColor = UIColor(red: 0/255, green: 85/255, blue: 187/255, alpha: 1)
+        tabBar.backgroundColor = .white
+        tabBar.barTintColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+
+        
+        setViewControllers([vc1], animated: true)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
