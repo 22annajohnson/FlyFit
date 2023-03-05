dbb5ec9963bc91a4649de0ab13277f8c84096514
Anna Johnson <22annajohnson@gmail.com>
2023-03-05 13:40:13 -0500

    Fixed Account icon in header view
    -Elements:
    	-Added additional view behind the accountView (UIImageView) in order to change the color of the person in the icon.
    	-Moved the frame CGRect()s into the declaration of each element
    	-Resized icon to match Figma file
    
    -Style:
    	-Added accountBackgroundColor to style guide to change foreground color of account icon

--- FlyFit/StyleGuide/Elements.swift
+++ FlyFit/StyleGuide/Elements.swift
@@ -15,25 +15,28 @@ class AddElements: ObservableObject {
     }
     
     func addHeader(title: String) -> UIView {
-        let header = UIView()
-        let label = UILabel()
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 144))
+        let label = UILabel(frame: CGRect(x: header.bounds.width*0.05, y: header.bounds.height*0.40, width: 200, height: 34))
         let accountIcon = UIImage(systemName: "person.crop.circle.fill")
-        let accountView = UIImageView()
+        let accountView = UIImageView(frame: CGRect(x: header.bounds.width*0.85, y: header.bounds.height*0.40, width: 34.85, height: 34.85))
+        let accountBackgroundView = UIView(frame: CGRect(x: (header.bounds.width*0.85)+4, y: (header.bounds.height*0.40)+3, width: 28, height: 28))
         
-        header.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 144)
         header.backgroundColor = Style().secondaryBackgroundColor
         
         label.text = title
-        label.frame = CGRect(x: header.bounds.width*0.05, y: header.bounds.height*0.40, width: 200, height: 34)
         label.font = Style().headingFont
         label.textColor = Style().primaryTextColor
         
-        accountView.frame = CGRect(x: header.bounds.width*0.85, y: header.bounds.height*0.40, width: 38.45, height: 38.45)
         accountView.image = accountIcon
-        accountView.tintColor = Style().primaryTextColor
+        accountView.tintColor = Style().accountBackgroundColor
+        accountView.layer.cornerRadius = 34.85/2
+        
+        accountBackgroundView.backgroundColor = Style().primaryTextColor
+        accountBackgroundView.layer.cornerRadius = 28/2
         
         
         header.addSubview(label)
+        header.addSubview(accountBackgroundView)
         header.addSubview(accountView)
         
         return header
--- FlyFit/StyleGuide/Style.swift
+++ FlyFit/StyleGuide/Style.swift
@@ -12,6 +12,7 @@ struct Style {
     var primaryBackgroundColor = UIColor(red: 29/255, green: 38/255, blue: 37/255, alpha: 1)
     var secondaryBackgroundColor = UIColor(red: 58/255, green: 81/255, blue: 84/255, alpha: 1)
     var primaryTextColor = UIColor(red: 229/255, green: 239/255, blue: 249/255, alpha: 1)
+    var accountBackgroundColor = UIColor(red: 146/255, green: 154/255, blue: 162/255, alpha: 1)
     var urgentColor = UIColor(red: 196/255, green: 30/255, blue: 30/255, alpha: 1)
     var warningColor = UIColor(red: 195/255, green: 80/255, blue: 30/255, alpha: 1)
     var accentColor = UIColor(red: 80/255, green: 149/255, blue: 158/255, alpha: 1)
